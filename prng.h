49b03c8fb877802012d37ba48a29764f43167157
Hugo Benichi <hugobenichi@gmail.com>
2017-01-04 19:33:01 +0900

    add 64 bits xorshift prng

---
+++ prng.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <stdint.h>
+
+// a reimplementation of http://xorshift.di.unimi.it/
+// TODO:
+//		implement MT19937-64 ?
+//		add jump functions
+//    pass them through http://simul.iro.umontreal.ca/testu01/tu01.html
+//    add xorshift128+, xorshift1024*
+
+// TODO: use custom int types header instead
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef uint64_t u64;
+typedef uint32_t u32;
+
+typedef float f32;
+typedef double f64;
+
+// TODO: consider renaming to prng_t, or prng_t1
+typedef u64 xorshift64s;
+
+// TODO: test this
+u64 prng_next(xorshift64s* r) {
+	u64 u = *r;
+	u ^= u >> 12;
+	u ^= u << 25;
+	u ^= u >> 27;
+	*r = u;
+	return u * 2685821657736338717;
+}
+
+static const f64 f = 1. / (1L << 52);
+static const u64 m = (1L << 52) - 1;
+
+f64 prng_f64_of(u64 x) {
+  // TODO optimize this by removing the float multiplicastion
+  return f * (f64) (x & m);
+}
---
+++ prng_main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include "./prng.h"
+
+int main(int n, char ** args) {
+  xorshift64s r1 = 42;
+  xorshift64s r2 = r1 + 1;
+
+  puts("r1:");
+  for (int i = 0; i < 30; i++) {
+    u64 x = prng_next(&r1);
+    printf("%e   %ld\n", prng_f64_of(x), x);
+  }
+
+  puts("\nr2:");
+
+  for (int i = 0; i < 30; i++) {
+    u64 x = prng_next(&r2);
+    printf("%e   %ld\n", prng_f64_of(x), x);
+  }
+}
